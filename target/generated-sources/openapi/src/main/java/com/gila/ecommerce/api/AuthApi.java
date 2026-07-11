3f6c160ed1df5be40fd08f52fd0817101d4ba22b
LeobasCordero <leocorderoguzman@gmail.com>
2026-07-10 22:20:56 -0600

    feat: generate Java DTOs and API interfaces from OpenAPI specification

--- target/generated-sources/openapi/src/main/java/com/gila/ecommerce/api/AuthApi.java
+++ target/generated-sources/openapi/src/main/java/com/gila/ecommerce/api/AuthApi.java
@@ -33,7 +33,7 @@ import java.util.Map;
 import java.util.Optional;
 import jakarta.annotation.Generated;
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-07-10T21:59:44.763631900-06:00[GMT-06:00]", comments = "Generator version: 7.4.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-07-10T22:17:03.848042900-06:00[GMT-06:00]", comments = "Generator version: 7.4.0")
 @Validated
 @Tag(name = "Auth", description = "the Auth API")
 public interface AuthApi {
