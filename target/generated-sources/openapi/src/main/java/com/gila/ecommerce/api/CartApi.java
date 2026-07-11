2f7c26c8c73c865ae8bce8dfec861b43740f59d0
LeobasCordero <leocorderoguzman@gmail.com>
2026-07-10 23:05:38 -0600

    feat: generate OpenAPI Spring interfaces and DTOs for ecommerce API endpoints

--- target/generated-sources/openapi/src/main/java/com/gila/ecommerce/api/CartApi.java
+++ target/generated-sources/openapi/src/main/java/com/gila/ecommerce/api/CartApi.java
@@ -34,7 +34,7 @@ import java.util.Map;
 import java.util.Optional;
 import jakarta.annotation.Generated;
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-07-10T22:56:55.704475600-06:00[GMT-06:00]", comments = "Generator version: 7.4.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-07-10T23:03:24.260209300-06:00[GMT-06:00]", comments = "Generator version: 7.4.0")
 @Validated
 @Tag(name = "Cart", description = "the Cart API")
 public interface CartApi {
--- target/generated-sources/openapi/src/main/java/com/gila/ecommerce/dto/CartItemRequestDto.java
+++ target/generated-sources/openapi/src/main/java/com/gila/ecommerce/dto/CartItemRequestDto.java
@@ -19,7 +19,7 @@ import jakarta.annotation.Generated;
  * CartItemRequestDto
  */
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-07-10T22:56:55.704475600-06:00[GMT-06:00]", comments = "Generator version: 7.4.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-07-10T23:03:24.260209300-06:00[GMT-06:00]", comments = "Generator version: 7.4.0")
 public class CartItemRequestDto {
 
   private UUID productId;
