fd1202f171da57590130f18cecb2c628ca706ce2
LeobasCordero <leocorderoguzman@gmail.com>
2026-07-10 22:52:26 -0600

    feat: generate OpenAPI client interfaces and DTOs for ecommerce API services

--- target/generated-sources/openapi/src/main/java/com/gila/ecommerce/dto/LoginRequestDto.java
+++ target/generated-sources/openapi/src/main/java/com/gila/ecommerce/dto/LoginRequestDto.java
@@ -18,7 +18,7 @@ import jakarta.annotation.Generated;
  * LoginRequestDto
  */
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-07-10T22:39:40.388469-06:00[GMT-06:00]", comments = "Generator version: 7.4.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-07-10T22:50:57.121248300-06:00[GMT-06:00]", comments = "Generator version: 7.4.0")
 public class LoginRequestDto {
 
   private String username;
